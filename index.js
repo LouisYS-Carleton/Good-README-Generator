4c3fa54bc8005d98800ef4aba769ccaadbcfcbfe
Louis Yacksmith <louisyscarleton@gmail.com>
2020-10-10 20:41:59 -0400

    Added basic outline and package.json files

---
+++ index.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you from?'
+      },
+      {
+        type: 'input',
+        name: 
+        'hobby',
+        message: 'What is your favourite hobby?'
+      },
+      {
+        type: 'input',
+        name: 'food',
+        message: 'What is your favourite food?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+      },
+      {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.'
+      }
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
